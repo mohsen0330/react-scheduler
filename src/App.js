144260b0a8eab4304c2c26ef01524d9328913b9f
mohsen0330 <61183617+mohsen0330@users.noreply.github.com>
2021-10-04 19:45:23 +0800

    v1
    Library working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import { render } from '@testing-library/react';
+import { Inject,ScheduleComponent,Day, Week, WorkWeek, Month, Agenda } from '@syncfusion/ej2-react-schedule'
+
+
+function App() {
+  return (
+    <div className='App'>
+      <ScheduleComponent>
+            <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
+        </ScheduleComponent>
+    </div>
+  )
+}
+
+export default App;
+
+/*
 import React from 'react';
 import './App.css'
-import { Inject,ScheduleComponent,Day, Week, WorkWeek, Month, Agenda } from '@syncfusion/ej2-react-schedule'
+
 
 class App extends React.Component {
     public render() {
-        return <ScheduleComponent>
-            <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
-        </ScheduleComponent>
+        return 
     }
 }
+*/
